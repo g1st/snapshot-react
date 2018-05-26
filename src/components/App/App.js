1a9b383f945682f9d00051d85fb3861ecd3babf0
g1st <gintstan@gmail.com>
2018-05-26 20:47:59 +0100

    basic app structure

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import NavBar from '../NavBar';
+
+import './index.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+      </div>
+    );
+  }
+}
+
+export default App;
