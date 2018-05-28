27b813211811befcc518e5f7c0f1f96a2e0337df
g1st <gintstan@gmail.com>
2018-05-28 21:33:35 +0100

    add Button component

---
+++ src/components/Button/styled.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+import { colorMainButton, colorText } from '../../styles/variables';
+
+export const MainButton = styled.button`
+  display: inline-block;
+  background-color: ${colorMainButton};
+  padding: 14px 32px;
+  letter-spacing: 3px;
+  font-size: 1.4rem;
+  display: inline-block;
+  font-weight: 300;
+  margin: 0 auto;
+  color: #fff;
+  outline: none;
+  text-transform: capitalize;
+  border-radius: 5px;
+  border: none;
+  transition: all 0.3s;
+  cursor: pointer;
+
+  :hover {
+    color: ${colorText};
+    background-color: ${darken(0.5, `${colorMainButton}`)};
+  }
+`;
+MainButton.displayName = 'MainButton';
