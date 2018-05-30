1bda913f67c2cacd157fef6b3147507194108d50
g1st <gintstan@gmail.com>
2018-05-30 20:55:10 +0100

    add white bg color

--- src/styles/variables.js
+++ src/styles/variables.js
@@ -3,6 +3,7 @@ export const colorMainLight = '#304451';
 export const colorOverflowBg = '#304352';
 export const colorText = '#e2e2e2';
 export const colorHeadingText = '#f0f0f0';
+export const colorWhiteBg = '#f0f0f0';
 export const colorMainButton = '#e44c65';
 
 export const maxContentWidth = '1200px';
