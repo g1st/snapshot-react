c7b40b2c0600a7bffe50d77d5cfd962bd0654e53
g1st <gintstan@gmail.com>
2018-06-03 20:14:58 +0100

    add Form component

---
+++ src/components/Contact/Form/Form.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+
+import Button from '../../Button';
+import { Wrapper, FormContainer, Row, Name, Email, Message } from './styled';
+
+class Form extends Component {
+  state = {
+    message: '',
+    name: '',
+    email: '',
+    width: window.innerWidth
+  };
+
+  componentDidMount() {
+    window.addEventListener('resize', this.handleResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize);
+  }
+
+  handleResize = e => {
+    const windowWidth = e.target.innerWidth;
+
+    this.setState(() => ({
+      width: windowWidth
+    }));
+  };
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    alert(`Comment was sent: ${this.state.message}`);
+  }
+
+  render() {
+    return (
+      <Wrapper>
+        <FormContainer onSubmit={e => this.handleSubmit(e)}>
+          <Row>
+            <Name
+              required
+              type="text"
+              name="name"
+              placeholder="Name"
+              value={this.state.name}
+              onChange={e => this.handleChange(e)}
+            />
+            <Email
+              required
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={this.state.email}
+              onChange={e => this.handleChange(e)}
+            />
+          </Row>
+          <Message
+            name="message"
+            value={this.state.message}
+            placeholder="Message"
+            rows="8"
+            onChange={e => this.handleChange(e)}
+            required
+          />
+          <Button
+            type="submit"
+            big={this.state.width < 768}
+            title="Send message"
+          />
+        </FormContainer>
+      </Wrapper>
+    );
+  }
+}
+
+export default Form;
