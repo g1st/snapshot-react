a82e71f9f3729f6e3c97fd0dd59af52bba9e18ae
g1st <gintstan@gmail.com>
2018-06-03 20:13:58 +0100

    add contact component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,6 +5,7 @@ import Hero from '../Hero';
 import About from '../About';
 import TeamCarousel from '../TeamCarousel';
 import Gallery from '../Gallery';
+import Contact from '../Contact';
 import './index.css';
 
 class App extends Component {
@@ -16,6 +17,7 @@ class App extends Component {
         <About />
         <TeamCarousel />
         <Gallery />
+        <Contact />
       </div>
     );
   }
