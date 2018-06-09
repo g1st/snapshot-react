4622e901db614c6def13c6987987ae78a2296928
g1st <gintstan@gmail.com>
2018-06-09 11:07:43 +0100

    add snapshot

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import App from '.';
+
+describe('App Component', () => {
+  it('matches snapshot', () => {
+    const tree = shallow(
+      <App onEnter={() => {}} onLeave={() => {}} topOffset={0} />
+    );
+    expect(toJson(tree)).toMatchSnapshot();
+  });
 });
